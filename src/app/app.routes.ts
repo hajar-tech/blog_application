6a90e7fed7fede3f716f5d5fe32e7c5931054d0c
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-04-23 15:02:18 +0100

    code navbare && routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'   
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'post-list',
+        component: PostListComponent
+    },
+    {
+        path: 'post-detail',
+        component: PostDetailComponent
+    }
+];
