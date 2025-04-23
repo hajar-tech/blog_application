1832a32404674c23719ca54026e8312a7b3e4e19
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-04-23 15:29:32 +0100

    architecture de projet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { PostListComponent } from './post-list/post-list.component';
-import { PostDetailComponent } from './post-detail/post-detail.component';
+import { HomeComponent } from './features/pages/home/home.component';
+import { PostListComponent } from './features/posts/post-list/post-list.component';
+import { PostDetailComponent } from './features/posts/post-detail/post-detail.component';
 
 export const routes: Routes = [
     {
